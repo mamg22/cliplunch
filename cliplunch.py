d3100f3311701444cbe3215ab5bf32402389137e
mamg22 <45301823+mamg22@users.noreply.github.com>
2019-07-21 22:39:20 -0400

    Add Launch_item class

--- cliplunch.py
+++ cliplunch.py
@@ -2,27 +2,33 @@
 
 import subprocess
 import shlex
-import typing
+from typing import IO, List
 
 import pyperclip
 
-class launch_item:
-    # not used still, need to decide a config format
-    def __init__(self, name, command, options={}):
+class Launch_item:
+    def __init__(self, name: str, command: str, data:dict={}):
         self.name = name
         self.command = command
-        self.options = options
+        self.data = data
 
-def parse_config(conf: typing.IO[str]) -> dict:
-    associations = {}
+    def set_data(self, key:str, value) -> None:
+        self.data[key] = value
+
+def parse_config(conf: IO[str]) -> List[Launch_item]:
+    associations = []
     for line in conf:
         try:
-            name, command = map(lambda section: section.strip(), line.split("|", 1))
-            associations[name] = command
+            sections = list(map(lambda section: section.strip(), line.split("|", 1)))
+            associations.append(Launch_item(sections[0], sections[1]))
         except ValueError as ex:
             pass
     return associations
 
-def launch(command: str, stderr: typing.IO[str]=subprocess.DEVNULL) -> None:
-    subprocess.Popen(command + ' ' + shlex.quote(pyperclip.paste().strip()), stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=stderr, shell=True)
+def launch(launch_item: Launch_item, stderr: IO[str]=subprocess.DEVNULL) -> None:
+    subprocess.Popen(launch_item.command + ' ' + shlex.quote(pyperclip.paste().strip()), 
+                     stdin=subprocess.DEVNULL, 
+                     stdout=subprocess.DEVNULL, 
+                     stderr=stderr, 
+                     shell=True)
 
--- clipluncher.py
+++ clipluncher.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
+from typing import List
+
 import cliplunch
 
 conf_file = open("clip.lunch", "r")
-
 assoc = cliplunch.parse_config(conf_file)
 
-options = {}
-for index, name in enumerate(assoc, 1):
-    options[index] = name
-    print(f"{index}. {name}")
+for i in range(len(assoc)):
+    assoc[i].data["index"] = i + 1
+    print(f"{assoc[i].data['index']}. {assoc[i].name}")
 
 selection = int(input("Enter your choice: "))
 
-stderr = open("/tmp/cliplunch.err", "w+") 
+def find_by_index(index:int, item_list:List[cliplunch.Launch_item]) -> cliplunch.Launch_item:
+    for elem in item_list:
+        if elem.data["index"] == index:
+            return elem
 
-cliplunch.launch(assoc[options[selection]], stderr)
+stderr = open("/tmp/cliplunch.err", "w+") 
+cliplunch.launch(find_by_index(selection, assoc), stderr)
